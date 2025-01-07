c0e12504e5261be7f68a0910fe85f16e09ecaeca
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-01-07 09:41:31 -0500

    kept original config at vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/react-portfolio/', // Add this line
-});
+  plugins: [react()],
+})
