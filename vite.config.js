9e7ed098eb40c3f8a5dccc9fca592f5fc4b04162
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-01-06 23:41:19 -0500

    deployment third try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Replace 'your-repo-name' with your GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: 'react-portfolio', // This is important for GitHub Pages
+  base: '/react-portfolio/', // This is important for GitHub Pages
 });
