ca2c74cde6179b0611f0322e872359acafd95554
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-01-07 09:24:01 -0500

    deleted  type: module from package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Replace 'your-repo-name' with your GitHub repository name
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-portfolio/', // This is important for GitHub Pages
-});
+})
